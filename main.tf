50e3cf381e31b1ce7d1178f8540ba41bbd0405ea
kalyani <kalyani@gmail.com>
2024-12-02 20:50:01 +0530

    adding secret key and access key

--- main.tf
+++ main.tf
@@ -16,4 +16,6 @@ terraform {
 provider "aws" {
   region =  "us-east-1"
   profile = "default"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- variables.tf
+++ variables.tf
@@ -99,3 +99,15 @@ variable "use_public_ip" {
   type = bool
   default = false
 }
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
